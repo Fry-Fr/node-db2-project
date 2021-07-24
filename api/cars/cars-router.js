219c8787cb9b590c3fe3d377a7a91c662798e083
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-07-23 22:23:18 -0400

    endpoints for cars-router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,29 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+const Cars = require('./cars-model');
+
+router.get('/', (req, res, next) => {
+    Cars.getAll()
+        .then(cars => {
+            res.json(cars)
+        })
+        .catch(err => next(err))
+});
+
+router.get('/:id', (req, res, next) => {
+    const { id } = req.params;
+    Cars.getById(id)
+        .then(car => {
+            res.json(car)
+        })
+        .catch(err => next(err))
+});
+
+router.post('/', (req, res, next) => {
+    Cars.create(req.body)
+        .then(newCar => {
+            res.status(201).json(newCar)
+        })
+        .catch(err => next(err))
+});
+
+module.exports = router;
