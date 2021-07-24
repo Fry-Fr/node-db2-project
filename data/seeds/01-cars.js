e113cb7335563d9833dd94f2a3e109b59b10b383
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-07-23 22:26:07 -0400

    using knex for sqlite3 db

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,23 @@
-// STRETCH
+exports.seed = function(knex) {
+    return knex('cars').truncate()
+      .then(function () {
+        return knex('cars').insert([
+            {
+                vin: "N234651N2",
+                make: "Honda",
+                model: "Accord",
+                mileage: 120000,
+                title: null,
+                transmission: "automatic"
+            },
+            {
+                vin: "Y344676N2",
+                make: "Honda",
+                model: "Civic",
+                mileage: 177000,
+                title: null,
+                transmission: "manual"
+            },
+        ]);
+      });
+  };  
